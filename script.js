4cc235bf5fd5955084c5104b9028ae33fc0f9b5e
itsmeramtheboss <ram.nivas29@gmail.com>
2026-03-29 22:31:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3194,7 +3194,18 @@ doc.text(`OUT OF ${item.stats}`, centerX, yPos + 12, { align: "center" });
     
     yPos += boxHeight + 10;
 }
+    // Disclaimer at the Bottom of the MARKSHEET 
+    doc.setFontSize(5);
+    doc.setTextColor(0); // Ensure title is Black
+    doc.setFont("helvetica", "italic");doc.text("Disclaimer :  We are not responsible for any inadvertent error that may have crept in the Data being published on Net. This Marksheet is for immediate information to the examinees and CANNOT be treated as original. Please verify from RBSE.", 105, 290, { align: "center" }); doc.setFontSize(8);
 
+	doc.setFontSize(8);
+	doc.setTextColor(150); // Light gray
+	doc.setFont("helvetica", "italic");
+	doc.text("www.instagram.com/RamNivas29", 8, 230, {
+	    //align: "center",
+	     angle: 90
+	});
 
     // --- PASTE YOUR ENTIRE DRAWING LOGIC HERE ---
     // Copy everything from inside your current `dlBtn.onclick = () => { ... }`
